cd9141d505e48e3487e8ef9bd154839e7a4f4d19
sprayzomb <oguzsarigul@protonmail.com>
2017-07-05 16:41:38 +0300

    Project initialized 5/7

--- GUI.py
+++ GUI.py
@@ -1,4 +1,8 @@
 import tkinter as tk
+from tkinter import ttk
+import csv
+
+
 LARGE_FONT= ("Verdana", 12)
 
 class PR(tk.Tk):
@@ -6,16 +10,18 @@ class PR(tk.Tk):
     def __init__(self, *args, **kwargs):
 
         tk.Tk.__init__(self, *args, **kwargs)
+
+        tk.Tk.iconbitmap(self, default="logo.ico")
+        tk.Tk.wm_title(self, "Pr Project")
+
         container = tk.Frame(self)
-
         container.pack(side="top", fill="both", expand= True)
-
         container.grid_rowconfigure(0, weight=1)
         container.grid_columnconfigure(0, weight=1)
 
         self.frames = {}
 
-        for F in (StartPage, PageOne, PageTwo):
+        for F in (StartPage, Properties, PageTwo):
 
             frame = F(container, self)
 
@@ -30,7 +36,6 @@ class PR(tk.Tk):
         frame = self.frames[cont]
         frame.tkraise()
 
-
 class StartPage(tk.Frame):
 
     def __init__(self, parent, controller):
@@ -38,28 +43,40 @@ class StartPage(tk.Frame):
         label = tk.Label(self, text="Start Page", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1=tk.Button(self, text="Visit Page 1",
-                          command=lambda: [controller.show_frame(PageOne)])
+        button1=ttk.Button(self, text="Visit Properties",
+                          command=lambda: [controller.show_frame(Properties)])
         button1.pack()
 
-        button2 = tk.Button(self, text="Page Two",
-                            command=lambda: [controller.show_frame(PageTwo)])
+        button2 = ttk.Button(self, text="Visit PageTwo",
+                             command=lambda: [controller.show_frame(PageTwo)])
         button2.pack()
 
-class PageOne(tk.Frame):
+
+class Properties(tk.Frame):
 
     def __init__(self, parent, controller):
         tk.Frame.__init__(self, parent)
-        label = tk.Label(self, text="Page One", font=LARGE_FONT)
+        label = tk.Label(self, text="Properties", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Back to Home",
+        button1 = ttk.Button(self, text="Back to Home",
                             command=lambda: [controller.show_frame(StartPage)])
         button1.pack()
 
-        button2 = tk.Button(self, text="Page Two",
+        button2 = ttk.Button(self, text="Page Two",
                             command=lambda: [controller.show_frame(PageTwo)])
         button2.pack()
+
+        with open('compounds.csv', 'r') as f:
+            reader = csv.reader(f)
+            compound_list = list(reader)
+
+        compound = tk.StringVar()
+        box = ttk.Combobox(self, textvariable = compound,
+                           state='readonly', width=8)
+        box['values'] = (compound_list)
+        box.current(0)
+        box.pack()
 
 
 class PageTwo(tk.Frame):
@@ -69,18 +86,13 @@ class PageTwo(tk.Frame):
         label = tk.Label(self, text="Page Two", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = tk.Button(self, text="Back to Home",
+        button1 = ttk.Button(self, text="Back to Home",
                             command=lambda: [controller.show_frame(StartPage)])
         button1.pack()
 
-        button2 = tk.Button(self, text="Page One",
-                            command=lambda: [controller.show_frame(PageOne)])
+        button2 = ttk.Button(self, text="To Properties",
+                            command=lambda: [controller.show_frame(Properties)])
         button2.pack()
-
-
-
-
-
 
 
 app = PR()
