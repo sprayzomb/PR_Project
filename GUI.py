1bea7a594848512f930f7207c7bc3bebe82865a9
sprayzomb <oguzsarigul@protonmail.com>
2017-07-06 18:14:46 +0300

    GUI still in progress, autocompletion is added but user still can add un related things into the box.

--- GUI.py
+++ GUI.py
@@ -6,6 +6,9 @@ with open('compounds.csv', 'r') as f:
   reader = csv.reader(f)
   compound_list = list(reader)
   compound_list.sort()
+  compound_list = [l[0] for l in compound_list]
+
+print(compound_list)
 
 LARGE_FONT= ("Verdana", 12)
 
@@ -71,12 +74,17 @@ class Properties(tk.Frame):
                             command=lambda: [controller.show_frame(PageTwo)])
         button2.pack()
 
-        compound_1 = tk.StringVar()
-        box_1 = ttk.Combobox(self, textvariable=compound_1,
-                             state='readonly', width=30)
-        box_1['values'] = (compound_list)
-        box_1.current(0)
+        #compound_1 = tk.StringVar()
+        #box_1 = ttk.Combobox(self, textvariable=compound_1,
+        #                     state='readonly', width=30)
+        #box_1['values'] = (compound_list)
+        #box_1.current(0)
+        #box_1.pack()
+        box_1 = AutocompleteCombobox(self)
+        box_1.set_completion_list(compound_list)
         box_1.pack()
+        box_1.focus_set()
+
 
 
         compound_2 = tk.StringVar()
@@ -114,6 +122,58 @@ class PageTwo(tk.Frame):
                             command=lambda: [controller.show_frame(Properties)])
         button2.pack()
 
+class AutocompleteCombobox(ttk.Combobox):
+
+        def set_completion_list(self, completion_list):
+            """Use our completion list as our drop down selection menu, arrows move through menu."""
+            self._completion_list = sorted(completion_list, key=str.lower)  # Work with a sorted list
+            self._hits = []
+            self._hit_index = 0
+            self.position = 0
+            self.bind('<KeyRelease>', self.handle_keyrelease)
+            self['values'] = self._completion_list  # Setup our popup menu
+
+        def autocomplete(self, delta=0):
+            """autocomplete the Combobox, delta may be 0/1/-1 to cycle through possible hits"""
+            if delta:  # need to delete selection otherwise we would fix the current position
+                self.delete(self.position, tk.END)
+            else:  # set position to end so selection starts where textentry ended
+                self.position = len(self.get())
+            # collect hits
+            _hits = []
+            for element in self._completion_list:
+                if element.lower().startswith(self.get().lower()):  # Match case insensitively
+                    _hits.append(element)
+            # if we have a new hit list, keep this in mind
+            if _hits != self._hits:
+                self._hit_index = 0
+                self._hits = _hits
+            # only allow cycling if we are in a known hit list
+            if _hits == self._hits and self._hits:
+                self._hit_index = (self._hit_index + delta) % len(self._hits)
+            # now finally perform the auto completion
+            if self._hits:
+                self.delete(0, tk.END)
+                self.insert(0, self._hits[self._hit_index])
+                self.select_range(self.position, tk.END)
+
+        def handle_keyrelease(self, event):
+            """event handler for the keyrelease event on this widget"""
+            if event.keysym == "BackSpace":
+                self.delete(self.index(tk.INSERT), tk.END)
+                self.position = self.index(tk.END)
+            if event.keysym == "Left":
+                if self.position < self.index(tk.END):  # delete the selection
+                    self.delete(self.position, tk.END)
+                else:
+                    self.position = self.position - 1  # delete one character
+                    self.delete(self.position, tk.END)
+            if event.keysym == "Right":
+                self.position = self.index(tk.END)  # go to end (no selection)
+            if len(event.keysym) == 1:
+                self.autocomplete()
+                # No need for up/down, we'll jump to the popup
+                # list at the position of the autocompletion
 
 app = PR()
 app.mainloop()
