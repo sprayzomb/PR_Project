fee26a90535ee63986319e71bd795bc5a04908fa
sprayzomb <oguzsarigul@protonmail.com>
2017-07-07 17:51:47 +0300

    GUI still in progress

--- GUI.py
+++ GUI.py
@@ -9,7 +9,7 @@ with open('compounds.csv', 'r') as f:
   compound_list.sort()
   compound_list = [l[0] for l in compound_list]
 
-print(compound_list)
+#print(compound_list)
 
 LARGE_FONT= ("Verdana", 12)
 
@@ -81,31 +81,25 @@ class Properties(tk.Frame):
         #box_1['values'] = (compound_list)
         #box_1.current(0)
         #box_1.pack()
-        box_1 = AutocompleteCombobox(self)
+        box_1 = AutocompleteCombobox(self, width=25)
         box_1.set_completion_list(compound_list)
         box_1.pack()
         box_1.focus_set()
 
-
-
-        compound_2 = tk.StringVar()
-        box_2 = ttk.Combobox(self, textvariable=compound_2,
-                           state='readonly', width=30)
-        box_2['values'] = (compound_list)
-        box_2.current(0)
+        box_2 = AutocompleteCombobox(self, width=25)
+        box_2.set_completion_list(compound_list)
         box_2.pack()
+        box_2.focus_set()
 
-        compound_3 = tk.StringVar()
-        box_3 = ttk.Combobox(self, textvariable=compound_3,
-                           state='readonly', width=30)
-        box_3['values'] = (compound_list)
-        box_3.current(0)
+        box_3 = AutocompleteCombobox(self, width=25)
+        box_3.set_completion_list(compound_list)
         box_3.pack()
+        box_3.focus_set()
 
-        box_4 = tk. Listbox(self)
-        for item in compound_list:
-            box_4.insert(tk.END, item)
+        box_4 = AutocompleteCombobox(self, width=25)
+        box_4.set_completion_list(compound_list)
         box_4.pack()
+        box_4.focus_set()
 
 
 class PageTwo(tk.Frame):
@@ -115,13 +109,41 @@ class PageTwo(tk.Frame):
         label = tk.Label(self, text="Page Two", font=LARGE_FONT)
         label.pack(pady=10, padx=10)
 
-        button1 = ttk.Button(self, text="Back to Home",
+        self.addButton = ttk.Button(self, text="Back to Home",
                             command=lambda: [controller.show_frame(StartPage)])
-        button1.pack()
+        self.addButton.pack()
 
-        button2 = ttk.Button(self, text="To Properties",
-                            command=lambda: [controller.show_frame(Properties)])
-        button2.pack()
+        #button2 = ttk.Button(self, text="To Properties",
+        #                    command=lambda: [controller.show_frame(Properties)])
+        #button2.pack()
+
+        self.create_widgets()
+
+    def create_widgets(self):
+        self.search_var = tk.StringVar()
+        self.search_var.trace("w", lambda name, index, mode: self.update_list())
+        self.entry = tk.Entry(self, textvariable=self.search_var, width=13)
+        self.lbox = tk.Listbox(self, width=45, height=15)
+        self.lbox.bind('<Double-Button-1>', lambda x: self.addButton.invoke())
+        self.entry.pack()
+        self.lbox.pack()
+
+        # Function for updating the list/doing the search.
+        # It needs to be called here to populate the listbox.
+        self.update_list()
+
+    def update_list(self):
+        search_term = self.search_var.get()
+
+
+        # Just a generic list to populate the listbox
+        lbox_list = compound_list
+
+        self.lbox.delete(0, tk.END)
+
+        for item in lbox_list:
+            if search_term.lower() in item.lower():
+                self.lbox.insert(tk.END, item)
 
 class AutocompleteCombobox(ttk.Combobox):
 
@@ -142,7 +164,9 @@ class AutocompleteCombobox(ttk.Combobox):
         def autocomplete(self, delta=0):
             """autocomplete the Combobox, delta may be 0/1/-1 to cycle through possible hits"""
             if delta:  # need to delete selection otherwise we would fix the current position
+
                 self.delete(self.position, tk.END)
+
             else:  # set position to end so selection starts where textentry ended
                 self.position = len(self.get())
             # collect hits
@@ -159,21 +183,29 @@ class AutocompleteCombobox(ttk.Combobox):
                 self._hit_index = (self._hit_index + delta) % len(self._hits)
             # now finally perform the auto completion
             if self._hits:
+
                 self.delete(0, tk.END)
                 self.insert(0, self._hits[self._hit_index])
+
                 self.select_range(self.position, tk.END)
 
         def handle_keyrelease(self, event):
             """event handler for the keyrelease event on this widget"""
             if event.keysym == "BackSpace":
+
                 self.delete(self.index(tk.INSERT), tk.END)
+
                 self.position = self.index(tk.END)
             if event.keysym == "Left":
                 if self.position < self.index(tk.END):  # delete the selection
+
                     self.delete(self.position, tk.END)
+
                 else:
                     self.position = self.position - 1  # delete one character
+
                     self.delete(self.position, tk.END)
+
             if event.keysym == "Right":
                 self.position = self.index(tk.END)  # go to end (no selection)
             if len(event.keysym) == 1:
